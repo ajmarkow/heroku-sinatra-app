1393d51fdcf765f105b5dfc6b9978fbf5c51d1e1
AJ Markow <alexanderjmarkow@gmail.com>
2020-10-02 11:16:01 -0700

    update app.rb

--- app.rb
+++ app.rb
@@ -1,7 +1,8 @@
-require "rubygems"
-require "sinatra"
-require "bundler"
-require "config"
+require("sinatra")
+require("sinatra/reloader")
+require("./lib/Dictionary")
+require("pry")
+also_reload("lib/**/*.rb")
 
 # Quick test
 get("/main") do
