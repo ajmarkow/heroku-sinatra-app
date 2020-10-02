6bffd379878d5e9eac2cee7575f427ad6672142e
AJ Markow <alexanderjmarkow@gmail.com>
2020-10-02 16:56:11 -0700

    Update individual word page.  Change code to try and get word pages by id working

--- app.rb
+++ app.rb
@@ -10,19 +10,23 @@ get("/") do
   erb(:mainpage)
 end
 
-get("/words/") do
-  "Shows a specific word based on id.  Use #{params[:id]}"
+get("/words") do
+  erb(:words)
 end
 
 post("/words") do
   @word = params[:wordtoadd]
   @definition = params[:definition]
-  word = Dictionary.new({ :name => "#{@word}", :definition => "#{@definition}", :wordid => "1" })
+  word = Dictionary.new({ :name => "#{@word}", :definition => "#{@definition}", :wordid => 1 })
   @dictionaries = Dictionary.all()
   word.save()
-  erb(:mainpage)
+  erb(:words)
 end
 
+get("words/:id") do
+  @word = Dictionary.find(params[:id].to_i())
+  erb(:word)
+end
 # get("words/:id/edit") do
 #   Page with form for updating album with an id of #{params[:id]}
 # end
